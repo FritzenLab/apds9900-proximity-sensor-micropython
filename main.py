b8e31a18200bef5e073c39d04611d92bc62341b6
Clovis Fritzen <clovisf@gmail.com>
2025-03-15 11:05:12 -0300

    edited file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# based on code from https://github.com/rlangoy/uPy_APDS9960/tree/master
 from machine import I2C, Pin
 from time import sleep_ms
 from apds9900LITE import APDS9900LITE
 
-#Init I2C Buss on RP2040
-i2c = I2C(0, scl=Pin(7), sda=Pin(6), freq=400_000)
+# If you need to figure the APDS9900 address (which is 0x39) use https://randomnerdtutorials.com/raspberry-pi-pico-i2c-scanner-micropython/
+#On ESP32-C3 Supermini the i2c pins are D4 (pin 6 SDA) and D5 (pin 7 SCL)
+i2c = I2C(0, scl=Pin(7), sda=Pin(6), freq=400_000) 
 onboardled= Pin(8, Pin.OUT)
 
 apds9900=APDS9900LITE(i2c)      # Enable sensor
